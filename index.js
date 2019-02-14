183e4902af8470136c5ea0e717f4f33a323db787
devstacker <allstacks.io@gmail.com>
2019-02-14 20:00:07 +0900

    @bable/node and ES6

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const express = require('express')
+import express from "express";
 const app = express()
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleHome = (req, res) => res.send("hello from home");
+const handleProfile = (req, res) => res.send("you are on my profile");
 
-function handleHome(req, res) {
-    //console.log(req);
-    res.send("hello from home");
-}
-
-function handleProfile(req, res) {
-    res.send("you are on my profile");
-}
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening); 
