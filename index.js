2be88eb4af72515c0b43f840840f486bb13da168
devstacker <allstacks.io@gmail.com>
2019-02-14 21:31:05 +0900

    middleware : morgan, helmet, cookie-parser, body-parser

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express()
 
 const PORT = 4000;
@@ -6,12 +10,12 @@ const PORT = 4000;
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("hello from home");
 const handleProfile = (req, res) => res.send("you are on my profile");
-const betweenHome = (req, res, next) => {
-    console.log("I'm between.");
-    next();
-}
 
-app.use(betweenHome);
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cookieParser);
+app.use(bodyParser.json());
+app.use(bodyParser.urlendcoded({ extended : true }));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening); 
