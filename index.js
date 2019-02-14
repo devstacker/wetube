52721984beb6c835cb27f7bf44dff2bd77ac9691
devstacker <allstacks.io@gmail.com>
2019-02-14 17:13:05 +0900

    GET method

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ const PORT = 4000;
 function handleListening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
-app.listen(PORT, handleListening);
+
+function handleHome(req, res) {
+    //console.log(req);
+    res.send("hello from home");
+}
+
+function handleProfile(req, res) {
+    res.send("you are on my profile");
+}
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(PORT, handleListening); 
