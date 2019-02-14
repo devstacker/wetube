659c59baea5bbcc0112b8fe2d6efe1700c13898f
devstacker <allstacks.io@gmail.com>
2019-02-14 21:01:25 +0900

    express middleware test

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const PORT = 4000;
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("hello from home");
 const handleProfile = (req, res) => res.send("you are on my profile");
+const betweenHome = (req, res, next) => {
+    console.log("I'm between.");
+    next();
+}
 
+app.use(betweenHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening); 
