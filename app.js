f1ab5f960238ea47b0637e74d4ca34ef58da738d
devstacker <allstacks.io@gmail.com>
2019-02-17 19:59:03 +0900

    express router

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 const app = express();
 
 const handleHome = (req, res) => res.send("hello from my home");
+
 const handleProfile = (req, res) => res.send("you are on my profile");
 
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
@@ -16,5 +18,7 @@ app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+
+app.use("/user", userRouter);
 
 export default app;
