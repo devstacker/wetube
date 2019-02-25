8a08d31da30de67fa9d6233e3735cad18105ffc4
devstacker <allstacks.io@gmail.com>
2019-02-25 16:42:07 +0900

    Using template engines - Pug

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 const app = express();
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 
+app.set('view engine', "pug");
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
