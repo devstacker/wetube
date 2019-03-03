4de7a04b634da79de05d203439a93c9454b27bc0
devstacker <allstacks.io@gmail.com>
2019-03-03 16:50:32 +0900

    users, vidoes forms

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -10,11 +10,10 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
 
