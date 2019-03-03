d9e5f35663c9ac168b1a363a29d1973378c96abb
devstacker <allstacks.io@gmail.com>
2019-03-03 16:52:49 +0900

    fake db & mixin

---
+++ db.js
@@ -0,0 +1,39 @@
+export const videos = [
+    {
+        id: 123123,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "devstacker",
+            email: "devstacker@email.com"
+        }
+    },
+    {
+        id: 232324,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "devstacker",
+            email: "devstacker@email.com"
+        }
+    },
+    {
+        id: 34536345,
+        title: 'Video perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "devstacker",
+            email: "devstacker@email.com"
+        }
+    }
+
+];
