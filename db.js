b69b8b3251e32c83693dc5ba5ecc21aeb22db42c
devstacker <allstacks.io@gmail.com>
2019-03-10 18:33:08 +0900

    Connection to MongoDB & Video, Comment Model

--- db.js
+++ db.js
@@ -1,39 +1,20 @@
-export const videos = [
-    {
-        id: 123123,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoUrl: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "devstacker",
-            email: "devstacker@email.com"
-        }
-    },
-    {
-        id: 232324,
-        title: 'Video super',
-        description: 'This is something I love',
-        views: 24,
-        videoUrl: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "devstacker",
-            email: "devstacker@email.com"
-        }
-    },
-    {
-        id: 34536345,
-        title: 'Video perfect',
-        description: 'This is something I love',
-        views: 24,
-        videoUrl: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "devstacker",
-            email: "devstacker@email.com"
-        }
-    }
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-];
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to MongoDB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
